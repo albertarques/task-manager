f2802e62505a69eab49705574dc6fd59cd1daeb0
Albert Arques <albertarques@gmail.com>
2025-02-14 16:32:44 +0100

    Add Task to User ManyToOne relationship fixtures

--- src/DataFixtures/TaskFixtures.php
+++ src/DataFixtures/TaskFixtures.php
@@ -4,10 +4,12 @@ namespace App\DataFixtures;
 
 use App\Entity\Task;
 use App\Entity\Enum\TaskStatusEnum;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class TaskFixtures extends Fixture
+class TaskFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
@@ -17,14 +19,33 @@ class TaskFixtures extends Fixture
             $task->setDescription("Descripción de la tarea $i");
             $task->setDueDate((new \DateTime())->modify("+$i days"));
 
-            // Selecciona un caso aleatorio de TaskStatusEnum
             $statuses = TaskStatusEnum::cases();
             $randomStatus = $statuses[array_rand($statuses)];
             $task->setStatus($randomStatus);
 
+            // Obtener y asignar un usuario aleatorio
+            $randomUser = $this->getRandomUserReference();
+
+            $task->setUser($randomUser);
             $manager->persist($task);
         }
 
         $manager->flush();
+    }
+
+    /**
+     * Devuelve una referencia de un usuario aleatorio.
+     */
+    private function getRandomUserReference(): User
+    {
+        $randomEmail = "user-" . random_int(1, 5) . "@example.com";
+        return $this->getReference($randomEmail, User::class);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UserFixtures::class,
+        ];
     }
 }
--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,15 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $passwordHasher;
+
+    public function __construct(UserPasswordHasherInterface $passwordHasher)
+    {
+        $this->passwordHasher = $passwordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
-        for ($i = 1; $i <= 10; $i++) {
+        for ($i = 1; $i <= 5; $i++) {
             $user = new User();
-            $user->setEmail("usuario$i@example.com");
-            $user->setPassword(password_hash("password$i", PASSWORD_BCRYPT));
+            $email = "user-$i@example.com";
+            $user->setEmail($email);
+            $user->setPassword($this->passwordHasher->hashPassword($user, 'password123'));
+
+            $this->addReference($email, $user);
 
             $manager->persist($user);
         }
