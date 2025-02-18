99513df22e3bcf9072089a6332f9077276ecb426
Albert Arques <albertarques@gmail.com>
2025-02-18 06:23:15 +0100

    Rename to Home Controller

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,18 +5,22 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Repository\TaskRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class RouteController extends AbstractController
+class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function index(TaskRepository $tr): Response
+    public function index(Security $security): Response
     {
+        $user = $security->getUser();
+
         return $this->render('/index.html.twig', [
             'controller_name' => 'RouteController',
             'title' => 'Home',
             'page' => 'home',
+            'user' => $user,
         ]);
     }
 }
